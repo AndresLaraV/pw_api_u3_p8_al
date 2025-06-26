ae02cda59058da43fa69e6ff4c5fd2cf7ea2a718
AndresLaraV <aslarav@uce.edu.ec>
2025-06-25 19:43:17 -0500

    Taller30

--- src/main/java/uce/edu/web/api/service/EstudianteServiceImpl.java
+++ src/main/java/uce/edu/web/api/service/EstudianteServiceImpl.java
@@ -1,20 +1,28 @@
 package uce.edu.web.api.service;
-
+ 
 import uce.edu.web.api.repository.modelo.Estudiante;
 import uce.edu.web.api.repository.IEstudianteRepo;
+ 
+import java.util.List;
+ 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-
+ 
 @ApplicationScoped
 public class EstudianteServiceImpl implements IEstudianteService {
-
+ 
     @Inject
     private IEstudianteRepo estudianteRepo;
-
+ 
     @Override
     public Estudiante buscarPorId(Integer id) {
         return this.estudianteRepo.seleccionarPorId(id);
-
+ 
     }
-
+ 
+    @Override
+    public List<Estudiante> buscarTodos() {
+        return this.estudianteRepo.seleccionarTodos();
+    }
+ 
 }
